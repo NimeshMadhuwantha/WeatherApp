08e545fcc21ce2d72f701aa0f3e7b5f45a81962d
NimeshMadhuwantha <janmadhuwantha@std.appsc.sab.ac.lk>
2024-07-14 20:31:39 +0530

    update weatherapp basic

---
+++ weatherapp.py
@@ -0,0 +1,27 @@
+from tkinter import *
+
+from geopy.geocoders import Nominatim
+from tkinter import ttk,messagebox
+from timezonefinder import TimezoneFinder
+from datetime import *
+import requests
+import pytz
+from PIL import Image, ImageTk
+import time
+
+class weatherClass:
+    def __init__(self,root):
+        self.root=root
+        self.root.geometry("700x400+325+150")
+        self.root.title("Weather App")
+        self.root.resizable(False,False)
+        self.root.config(bg='#f8ea28',bd=1)
+
+
+if __name__=="__main__":
+    root=Tk()
+    obj=weatherClass(root)
+    root.mainloop()
+
+
+
