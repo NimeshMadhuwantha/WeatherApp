52169a2d4ff746ad8e1a7134528c8175a8ae1173
NimeshMadhuwantha <janmadhuwantha@std.appsc.sab.ac.lk>
2024-07-15 12:56:40 +0530

    update

--- weatherapp.py
+++ weatherapp.py
@@ -15,7 +15,22 @@ class weatherClass:
         self.root.geometry("700x400+325+150")
         self.root.title("Weather App")
         self.root.resizable(False,False)
-        self.root.config(bg='#f8ea28',bd=1)
+        self.root.config(bg='white',bd=1)
+
+        self.photo_image=ImageTk.PhotoImage(file="weather.jpg")
+        self.lbl_photo_image=Label(self.root,image=self.photo_image,bd=0).place(x=0,y=0)
+
+
+        self.var_city=StringVar()
+        self.var_txtcity=StringVar()
+        self.var_latitude=StringVar()
+        self.var_tempreture=StringVar()
+        self.var_humidity=StringVar()
+        self.var_pressure=StringVar()
+        self.var_wind=StringVar()
+        self.var_des=StringVar()
+        self.var_localtime=StringVar()
+
 
 
 if __name__=="__main__":
