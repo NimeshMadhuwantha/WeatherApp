ec90c59fd02b34d13fab686def84f34090e9b384
NimeshMadhuwantha <janmadhuwantha@std.appsc.sab.ac.lk>
2024-07-19 16:43:04 +0530

    update weather images and finish

--- weatherapp.py
+++ weatherapp.py
@@ -22,6 +22,7 @@ class weatherClass:
 
 
         self.var_city=StringVar()
+        #self.var_city.set("Kandy")
         self.var_txtcity=StringVar()
         self.var_latitude=StringVar()
         self.var_tempreture=StringVar()
@@ -31,48 +32,48 @@ class weatherClass:
         self.var_des=StringVar()
         self.var_localtime=StringVar()
 
-        self.var_textcity=Label(self.root,text="",font=("times 30 bold"),fg='black',bg="#08f7f7",justify=CENTER)
-        self.var_textcity.place(x=0,y=50)
+        self.var_textcity=Label(self.root,text="",font=("times 30 bold"),fg='#ffffff',bg="#022a3a",justify=CENTER)
+        self.var_textcity.place(x=405,y=105)
 
-        self.var_latitude=Label(self.root,text="",font=("times 12 bold"),fg='black',bg="#08f7f7",justify=CENTER)
-        self.var_latitude.place(x=0,y=110)
+        self.var_latitude=Label(self.root,text="",font=("times 12 bold"),fg='#ffffff',bg="#022a3a",justify=CENTER)
+        self.var_latitude.place(x=510,y=160)
 
-        self.var_tempreture=Label(self.root,text="",font=("times 40 bold"),fg='black',bg="#08f7f7",justify=CENTER)
-        self.var_tempreture.place(x=20,y=150)
+        self.var_tempreture=Label(self.root,text="",font=("times 40 bold"),fg='#022a3a',bg="#ffffff",justify=CENTER)
+        self.var_tempreture.place(x=40,y=170)
 
-        self.var_localtime=Label(self.root,text="",font=("times 15 bold"),fg='black',bg="#08f7f7",justify=CENTER)
-        self.var_localtime.place(x=165,y=220,width=150)
+        self.var_localtime=Label(self.root,text="",font=("times 20 bold"),fg='#ffffff',bg="#022a3a",justify=CENTER)
+        self.var_localtime.place(x=400,y=230,width=250)
 
-        self.var_des=Label(self.root,text="",font=("times 14 bold"),fg='black',bg="#08f7f7",justify=CENTER)
-        self.var_des.place(x=80,y=330)
+        self.var_des=Label(self.root,text="",font=("times 12 bold"),fg='#022a3a',bg="#ffffff",justify=CENTER)
+        self.var_des.place(x=37,y=340)
 
-        self.var_wind=Label(self.root,text="",font=("times 14 bold"),fg='black',bg="#08f7f7",justify=CENTER)
-        self.var_wind.place(x=220,y=330)
+        self.var_wind=Label(self.root,text="",font=("times 12 bold"),fg='#022a3a',bg="#ffffff",justify=CENTER)
+        self.var_wind.place(x=170,y=340)
 
-        self.var_humidity=Label(self.root,text="",font=("times 14 bold"),fg='black',bg="#08f7f7",justify=CENTER)
-        self.var_humidity.place(x=300,y=330)
+        self.var_humidity=Label(self.root,text="",font=("times 12 bold"),fg='#022a3a',bg="#ffffff",justify=CENTER)
+        self.var_humidity.place(x=240,y=340)
 
-        self.var_pressure=Label(self.root,text="",font=("times 14 bold"),fg='black',bg="#08f7f7",justify=CENTER)
-        self.var_pressure.place(x=400,y=330)
+        self.var_pressure=Label(self.root,text="",font=("times 12 bold"),fg='#022a3a',bg="#ffffff",justify=CENTER)
+        self.var_pressure.place(x=320,y=340)
 
 
         #lable
 
-        label1=Label(self.root,text="DESCRIPTION",font=("times 10 bold"),fg='white',bg="#4c7b7b").place(x=80,y=307)
-        label2=Label(self.root,text="WIND",font=("times 10 bold"),fg='white',bg="#4c7b7b").place(x=220,y=307)
-        label3=Label(self.root,text="HUMIDITY",font=("times 10 bold"),fg='white',bg="#4c7b7b").place(x=300,y=307)
-        label4=Label(self.root,text="PRESSURE",font=("times 10 bold"),fg='white',bg="#4c7b7b").place(x=400,y=307)
+        label1=Label(self.root,text="DESCRIPTION",font=("times 10 bold"),fg='#022a3a',bg="#ffffff").place(x=37,y=320)
+        label2=Label(self.root,text="WIND",font=("times 10 bold"),fg='#022a3a',bg="#ffffff").place(x=170,y=320)
+        label3=Label(self.root,text="HUMIDITY",font=("times 10 bold"),fg='#022a3a',bg="#ffffff").place(x=240,y=320)
+        label4=Label(self.root,text="PRESSURE",font=("times 10 bold"),fg='#022a3a',bg="#ffffff").place(x=320,y=320)
         
 
-        textfield=Entry(self.root,textvariable=self.var_city,justify=CENTER,width=15,font=("times 25 bold"),bg='#545a5a',fg='white',bd=0)
-        textfield.place(x=383,y=35,width=225)
+        textfield=Entry(self.root,textvariable=self.var_city,justify=CENTER,width=15,font=("times 20 bold"),bg='#022a3a',fg='white',bd=0)
+        textfield.place(x=370,y=43,width=225)
         textfield.focus()
 
         #search button
 
         self.search_icon=PhotoImage(file="searchbtn.png")
         self.myimage_icon=Button(self.root,image=self.search_icon,borderwidth=0,command=self.getweather,cursor='hand2',bg='#203243',activebackground='#203243')
-        self.myimage_icon.place(x=612,y=40)
+        self.myimage_icon.place(x=624,y=42)
 
     def getweather(self):
         try:
@@ -93,7 +94,7 @@ class weatherClass:
             current_time=local_time.strftime("%I:%M:%p")
 
             #request date
-            #api="https://api.openweathermap.org/data/2.5/weather?q="+ city + "&appid=2d3b475655aa2d47bfa802b7103caff8"
+
             api="https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=2d3b475655aa2d47bfa802b7103caff8"
             weather_data=requests.get(api).json()
 
@@ -116,44 +117,33 @@ class weatherClass:
             if condition =='Rain':
                 print(condition=='Rain')
                 self.weather_image=PhotoImage(file="rain.png")
-                self.sign_image=Label(self.root,image=self.weather_image,bg='#203243',bd=0).place(x=450,y=100)
+                self.sign_image=Label(self.root,image=self.weather_image,bg='#ffffff',bd=0).place(x=50,y=35)
             elif condition == 'Clouds':
                 self.weather_image=PhotoImage(file="cloudy.png")
-                self.sign_image=Label(self.root,image=self.weather_image,bg='#203243',bd=0).place(x=450,y=100)
+                self.sign_image=Label(self.root,image=self.weather_image,bg='#ffffff',bd=0).place(x=50,y=35)
             elif condition == 'Clear':
                 self.weather_image=PhotoImage(file="clear.png")
-                self.sign_image=Label(self.root,image=self.weather_image,bg='#203243',bd=0).place(x=450,y=100)
+                self.sign_image=Label(self.root,image=self.weather_image,bg='#ffffff',bd=0).place(x=50,y=35)
             elif condition == 'Overcast':
                 self.weather_image=PhotoImage(file="overcast.png")
-                self.sign_image=Label(self.root,image=self.weather_image,bg='#203243',bd=0).place(x=450,y=100)
+                self.sign_image=Label(self.root,image=self.weather_image,bg='#ffffff',bd=0).place(x=50,y=35)
             elif condition == 'Smoke':
                 self.weather_image=PhotoImage(file="smoke.png")
-                self.sign_image=Label(self.root,image=self.weather_image,bg='#203243',bd=0).place(x=450,y=100)
+                self.sign_image=Label(self.root,image=self.weather_image,bg='#ffffff',bd=0).place(x=50,y=35)
             elif condition == 'Haze':
                 self.weather_image=PhotoImage(file="haze.png")
-                self.sign_image=Label(self.root,image=self.weather_image,bg='#203243',bd=0).place(x=450,y=100)
+                self.sign_image=Label(self.root,image=self.weather_image,bg='#ffffff',bd=0).place(x=50,y=35)
             elif condition == 'Mist':
-                self.weather_image=PhotoImage(file="Mist.png")
-                self.sign_image=Label(self.root,image=self.weather_image,bg='#203243',bd=0).place(x=450,y=100)
+                self.weather_image=PhotoImage(file="mist.png")
+                self.sign_image=Label(self.root,image=self.weather_image,bg='#ffffff',bd=0).place(x=50,y=35)
             elif condition == 'Snow':
                 self.weather_image=PhotoImage(file="snow.png")
-                self.sign_image=Label(self.root,image=self.weather_image,bg='#203243',bd=0).place(x=450,y=100)
+                self.sign_image=Label(self.root,image=self.weather_image,bg='#ffffff',bd=0).place(x=50,y=35)
                 
-
-            
-        
-
-
-
-
-
-
 
         except Exception as e:
             messagebox.showerror("Weather app", "Invalid Entry.....!")
-            print(e)
-
-    
+            print(e)  
 
 if __name__=="__main__":
     root=Tk()
